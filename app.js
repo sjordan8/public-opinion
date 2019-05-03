667b163a34ef28890bc19bb3fc410cfb6a055af2
Sean Jordan <seanj@liatrio.com>
2019-05-03 16:31:09 -0700

    Added city trend API call and trend search for tweets API call

--- app.js
+++ app.js
@@ -19,49 +19,6 @@ const T = new Twit({
 
 const app = express();
 
-app.use(express.static('static'));
-
-async function lookupCoords() {
-    let d = fs.readFileSync('cities.json');
-    let cities = JSON.parse(d);
-    
-    let promise = new Promise((res, rej) => {
-        let d = fs.readFileSync('cities.json');
-        let cities = JSON.parse(d);
-
-        for (let i = 0; i < cities.length; i++) {
-            let city = cities[i];
-            if(!city.hasOwnProperty("centroid")) {
-                T.get('geo/search', { query: city["name"], granularity: "city", max_results: "1" }, function (err, data, response) {
-                    if (err) {
-                        console.log("Hit the limit, probably");
-                    }
-                    else {
-                        let obj = data.result.places[0];
-                        city["centroid"] = obj["centroid"];
-                        console.log(city["centroid"]);
-                        cities[i] = city;
-                        console.log(cities[i]);
-                        let cityData = JSON.stringify(cities);
-                        fs.writeFile('cities.json', cityData, function(err) {
-                            console.log("Wrote city");
-                        });
-                    }
-                })
-            }
-        }
-        res(cities);
-    });
-
-    console.log("Waiting for the promise to complete");
-    let newCities = await promise;
-    console.log(newCities[0]);
-    let cityData = JSON.stringify(newCities);
-    fs.writeFile('cities.json', cityData, function(err) {
-        console.log("Wrote the new version of the cities");
-    });
-};
-
 app.get('/', function(req, res) {
     res.sendFile('index.html', { root: __dirname });
 });
@@ -71,39 +28,91 @@ app.get('/us', function(req, res) {
 });
 //Trends Function
 app.get('/get_cities', function(req, res) {
-    T.get('trends/available', function (err, data, response) {
-        var trends = data;
-        var cities = [];
-        for (var i = 0; i < trends.length - 1; i++) {
-            var trend = trends[i];
-
-            //console.log(trend['country']);
-            if (trend["country"] == "United States") {
-                var place = trend["placeType"];
-                if (place["name"] == "Town") {
+    T.get('trends/available', function (err, data, response)
+    {
+        let trends = data;
+        let cities = [];
+        for (let i = 0; i < trends.length - 1; i++)
+        {
+            let trend = trends[i];
+            if (trend["country"] == "United States")
+            {
+                let place = trend["placeType"];
+                if (place["name"] == "Town")
+                {
                     cities.push({ "name": trend["name"], "woeid": trend["woeid"] });
                 }
             }
         }
-        var cityData = JSON.stringify(cities);
-        fs.writeFile('cities.json', cityData, function(err) {
+        let cityData = JSON.stringify(cities);
+        fs.writeFile('cities.json', cityData, function(err)
+        {
             console.log("Written Cities!");
         });
         res.send(trends);
     })
 
-   /*
-    T.get('trends/place', { id: '2487956' }, function (err, data, response) {
-        res.send(data);
-    })*/
 });
 
 app.get('/get_coordinates', function(req, res) {
-    lookupCoords();
+
+    let d = fs.readFileSync('cities.json');
+    let cities = JSON.parse(d);
+
+    for (let i = 0; i < cities.length; i++)
+    {
+        let city = cities[i];
+        if(!city.hasOwnProperty("centroid"))
+        {
+            T.get('geo/search', { query: city["name"], granularity: "city", max_results: "1" }, function (err, data, response)
+            {
+                if (err)
+                    console.log(err);
+                else
+                {
+                    let obj = data.result.places[0];
+                    city["centroid"] = obj["centroid"];
+                    console.log(city["centroid"]);
+                    cities[i] = city;
+                    console.log(cities[i]);
+                    let cityData = JSON.stringify(cities);
+                    fs.writeFile('cities.json', cityData, function(err)
+                    {
+                        console.log("Wrote city");
+                    });
+                }
+            })
+        }
+    }
     res.send("Filling cities");
 });
+// Testing for San Francisco
+app.get('/get_trends_for_city', function(req, res) {
+    let topTen = [];
 
-        //console.log(data.result['places']);
+    T.get('trends/place', { id: '2487956' }, function (err, data, response)
+    {
+        let obj = data[0];
+        let trends = obj["trends"];
+
+        for (let i = 0; i < 10; i++)
+        {
+            let trend = trends[i];
+            topTen.push(trend);
+        }
+        console.log(topTen);
+        res.send(topTen);
+    })
+});
+
+app.get('/get_tweets_for_trend', function(req, res) {
+
+    T.get('tweets/search', { q: "%23Putin" } , function (err, data, response)
+    {
+        res.send(data);
+    })
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
